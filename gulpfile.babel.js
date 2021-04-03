59df585241ecae38326fc75274adf044702e6753
Darrel Masis <darrelmasis@gmail.com>
2021-04-02 20:35:12 -0600

    Se crearon las funciones que compilan SASS y JS en el gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,6 +13,56 @@ import browsersync from 'browser-sync'
 
 const server = browsersync.create()
 
+// Contiene las rutas de los archivos y el destino
+const path = {
+  styles: {
+    src: './dev/scss/styles.scss',
+    dest: './dist/css/'
+  },
+  scripts: {
+    src: './src/js/index.js',
+    dest: './dist/js/'
+  }
+}
+
+// Compila Sass minificado
+const styles = done => {
+  gulp.src(path.styles.src)
+  .pipe(sourcemaps.init())
+  .pipe(plumber())
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(postcss([autoprefixer()]))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(path.styles.dest))
+  .pipe(server.stream())
+  done()
+}
+
+// Compila Javascripts para producción
+const scripts = done => {
+  browserify(path.scripts.src)
+    .transform(babelify, {
+      global: true
+    })
+    .bundle()
+    .on('error', function (err) {
+      console.log(err)
+      this.emit('end')
+    })
+    .pipe(source('scripts.js'))
+    .pipe(buffer())
+    .pipe(minify({
+      ext: {
+        src: '-bundle.js',
+        min: '-min.js'
+      }
+    }))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path.scripts.dest))
+    done()
+}
+
 const serverUp = done => {
   server.init({
     server: {
@@ -20,6 +70,8 @@ const serverUp = done => {
     },
     open: false
   })
+  gulp.watch(path.styles.src, styles, server.reload)
+  gulp.watch(path.scripts.src, scripts, server.reload)
   gulp.watch('./*.html').on('change', server.reload)
   done()
 }
